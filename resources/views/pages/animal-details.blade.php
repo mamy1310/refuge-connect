cc51862a20a456c11c033ab795d7208fdbb84894
Rakotoarisoa Andrianirina Mamitiana <mamitiana_rakotoarisoa@yahoo.com>
2026-03-13 11:27:50 +0100

    Fix css

--- resources/views/pages/animal-details.blade.php
+++ resources/views/pages/animal-details.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <h1>{{ $animal->name }}</h1>
     <div class="animal-details">
-        <img src="{{ asset('images/animaux/' . $animal->photo) }}" alt="{{ $animal->name }}" class="animal-photo">
+        <img src="{{ asset('images/animaux/' . $animal->photo) }}" alt="{{ $animal->name }}" class="animal-details-photo">
         <div>
             <p>Espèce : {{ $animal->species }}</p>
             <p>Âge : {{ $animal->age }} ans</p>
